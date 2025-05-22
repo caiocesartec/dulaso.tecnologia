aa995f5f1addfea5467f396d1715533385eb5101
caiocesartec <newcesar25@gmail.com>
2025-05-22 20:26:53 -0300

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     const isMobile = /Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(
       navigator.userAgent
@@ -22,36 +19,23 @@ function App() {
 
   return (
     <>
-      <header className="headerImprovisado">
-        <p>
-          teste mobteste mobteste mobteste mobteste mobteste mobteste mobteste
-          mobteste mobteste mobteste mobteste mobteste mobteste mobteste
-          mobteste mobteste mobteste mobteste mobteste mobteste mobteste
-          mobteste mobteste mobteste mobteste mobteste mobteste mobteste
-          mobteste mobteste mobteste mobteste mobteste mobteste mobteste
-          mobteste mobteste mobteste mobteste mob
-        </p>
-      </header>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <main className="mainImprovisado">
+        <h1>
+          Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec
+          Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec
+          Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec
+          Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec
+          Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec Dulaso Tec
+          Dulaso Tec Dulaso Tec{" "}
+        </h1>
+        <a href="/" target="_blank">
+          <img
+            src={"/logos/dulaso/logoDulaso.svg"}
+            className="logo"
+            alt="Dulaso logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
     </>
   );
 }
