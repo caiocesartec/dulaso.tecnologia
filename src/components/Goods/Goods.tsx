c7fc76228a98eb8c5a2f57f3f9c063b15f1a9ff9
caiocesartec <newcesar25@gmail.com>
2025-05-23 07:37:20 -0300

    slider

--- src/components/Goods/Goods.tsx
+++ src/components/Goods/Goods.tsx
@@ -5,101 +5,98 @@ import {
   faEye,
   faHandshake,
 } from "@fortawesome/free-solid-svg-icons";
+
+// Import Swiper core and required modules
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-// import required modules
-import { Pagination } from "swiper/modules";
-
-export default function Goods() {
+const Goods: React.FC = () => {
   return (
-    <>
-      <section className="goodsSection">
-        <Swiper
-          slidesPerView={2}
-          centeredSlides={true}
-          spaceBetween={30}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            {" "}
-            <section className="box">
-              <FontAwesomeIcon
-                icon={faBullseye}
-                style={{ color: "#04F46B", fontSize: "34px" }}
-              />
-              <h3>Missão</h3>
-              <p>
-                Oferecer soluções tecnológicas acessíveis e inovadoras que
-                capacitem microempreendedores a{" "}
-                <strong>expandir seus negócios</strong>, aumentando sua
-                competitividade e presença digital de forma simples e eficiente.
-              </p>
-            </section>
-          </SwiperSlide>
+    <section className="goodsSection">
+      <Swiper
+        slidesPerView={2}
+        centeredSlides={true}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <section className="box">
+            <FontAwesomeIcon
+              icon={faBullseye}
+              style={{ color: "#04F46B", fontSize: "34px" }}
+            />
+            <h3>Missão</h3>
+            <p>
+              Oferecer soluções tecnológicas acessíveis e inovadoras que
+              capacitem microempreendedores a{" "}
+              <strong>expandir seus negócios</strong>, aumentando sua
+              competitividade e presença digital de forma simples e eficiente.
+            </p>
+          </section>
+        </SwiperSlide>
 
-          <SwiperSlide>
-            {" "}
-            <section className="box">
-              <FontAwesomeIcon
-                icon={faEye}
-                style={{ color: "#04F46B", fontSize: "34px" }}
-              />
-              <h3>Visão</h3>
-              <p>
-                Ser referência nacional em{" "}
-                <strong>transformação digital para microempreendedores</strong>,
-                promovendo strong inclusão tecnológica e impulsionando o{" "}
-                <strong>crescimento sustentável</strong> de pequenos e médios
-                negócios.
-              </p>
-            </section>
-          </SwiperSlide>
+        <SwiperSlide>
+          <section className="box">
+            <FontAwesomeIcon
+              icon={faEye}
+              style={{ color: "#04F46B", fontSize: "34px" }}
+            />
+            <h3>Visão</h3>
+            <p>
+              Ser referência nacional em{" "}
+              <strong>transformação digital para microempreendedores</strong>,
+              promovendo <strong>inclusão tecnológica</strong> e impulsionando o{" "}
+              <strong>crescimento sustentável</strong> de pequenos e médios
+              negócios.
+            </p>
+          </section>
+        </SwiperSlide>
 
-          <SwiperSlide>
-            {" "}
-            <section className="box">
-              <FontAwesomeIcon
-                icon={faHandshake}
-                style={{ color: "#04F46B", fontSize: "34px" }}
-              />
+        <SwiperSlide>
+          <section className="box">
+            <FontAwesomeIcon
+              icon={faHandshake}
+              style={{ color: "#04F46B", fontSize: "34px" }}
+            />
+            <h3>Valores</h3>
+            <ul>
+              <li>
+                <strong>Agilidade:</strong> Buscamos sempre soluções que
+                entregam o maior valor possível no menor tempo viável.
+              </li>
+              <li>
+                <strong>Compromisso:</strong> Trabalhamos com dedicação para o
+                sucesso dos microempreendedores.
+              </li>
+              <li>
+                <strong>Acessibilidade:</strong> Tornamos a tecnologia simples e
+                disponível para todos.
+              </li>
+              <li>
+                <strong>Ética:</strong> Atuamos com transparência, respeito e
+                responsabilidade.
+              </li>
+              <li>
+                <strong>Parceria:</strong> Valorizamos relações de confiança e
+                colaboração duradouras.
+              </li>
+            </ul>
+          </section>
+        </SwiperSlide>
+      </Swiper>
 
-              <h3>Valores</h3>
-              <ul>
-                <li>
-                  <strong>Agilidade:</strong> Buscamos sempre soluções que
-                  entragam o maior valor possivel no menor tempo viavel.
-                </li>
-                <li>
-                  <strong>Compromisso:</strong> Trabalhamos com dedicação para o
-                  sucesso dos microempreendedores.
-                </li>
-                <li>
-                  <strong>Acessibilidade:</strong> Tornamos a tecnologia simples
-                  e disponível para todos.
-                </li>
-                <li>
-                  <strong>Ética:</strong> Atuamos com transparência, respeito e
-                  responsabilidade.
-                </li>
-                <li>
-                  <strong>Parceria:</strong> Valorizamos relações de confiança e
-                  colaboração duradouras.
-                </li>
-              </ul>
-            </section>
-          </SwiperSlide>
-        </Swiper>
-
-        <section className="goodsSectionContainer"></section>
-      </section>
-    </>
+      <section className="goodsSectionContainer"></section>
+    </section>
   );
-}
+};
+
+export default Goods;
