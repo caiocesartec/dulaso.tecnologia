c5eba18acb91ccea2df99fe14239996b9a136e5b
caiocesartec <newcesar25@gmail.com>
2025-05-23 07:24:53 -0300

    jsons

--- src/components/Goods/Goods.tsx
+++ src/components/Goods/Goods.tsx
@@ -8,6 +8,7 @@ import {
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
 import { Pagination } from "swiper/modules";
