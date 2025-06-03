13389e904106aa2aa799791c4985cec3a972d182
caiocesartec <newcesar25@gmail.com>
2025-06-02 22:49:30 -0300

    well

--- src/components/Experts/Experts.tsx
+++ src/components/Experts/Experts.tsx
@@ -92,6 +92,14 @@ export default function Experts() {
 
         <SwiperSlide style={{ width: "auto" }}>
           <section>
+            <img src="/experts/well.jpeg" className="logo" alt="Daniel" />
+            <h3>Wellington Costa</h3>
+            <h4>E-commerce - Sites</h4>
+          </section>
+        </SwiperSlide>
+
+        <SwiperSlide style={{ width: "auto" }}>
+          <section>
             <img src="/experts/bax.png" className="logo" alt="Daniel" />
             <h3>Daniel</h3>
             <h4>Designer - UX</h4>
