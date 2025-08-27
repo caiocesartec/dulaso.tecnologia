0e7b094007304056e0249ac98a98d8793b0c786c
caiocesartec <newcesar25@gmail.com>
2025-08-27 16:27:18 -0300

    new fonts import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/reset.css";
 import "./styles/index.css";
 import App from "./App";
+import '@fontsource/bebas-neue' 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
